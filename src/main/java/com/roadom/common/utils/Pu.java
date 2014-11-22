250d3376661672cb22b28d7dac261394ca9f94df
R <roadom@hotmail.com>
2014-11-22 15:12:53 +0800

    log4jPu

---
+++ src/main/java/com/roadom/common/utils/Pu.java
@@ -0,0 +1,11 @@
+package com.roadom.common.utils;
+
+import org.apache.logging.log4j.util.PropertiesUtil;
+
+public class Pu {
+	public static void main(String[] args) {
+		
+		PropertiesUtil log4jPu = new PropertiesUtil("");
+		log4jPu.getClass();
+	}
+}

fec2661429937b3c7c48002df7b38d6bf7d0409e
R <roadom@hotmail.com>
2014-11-22 14:18:31 +0800

    first enum

--- src/main/java/com/roadom/customer/enums/CustomerSource.java
+++ src/main/java/com/roadom/customer/enums/CustomerSource.java
@@ -2,14 +2,28 @@ package com.roadom.customer.enums;
 
 import com.roadom.base.enums.BaseEnum;
 
-public enum CustomerSource {
-	website("主站");
+public enum CustomerSource implements BaseEnum {
+	website("主站"),
+	qq("QQ"),
+	douban("豆瓣"),
+	weibo("微博"),
+	baidu("百度"),
+	renren("人人"),
+	kaixin("开心")
+	;
 	
 	private String chineseName;
 	
 	CustomerSource(String cn){
 		chineseName = cn;
 	}
+
+	@Override
+	public String getChineseName() {
+		return this.chineseName;
+	}
+	
+	
 	
 	
 }
